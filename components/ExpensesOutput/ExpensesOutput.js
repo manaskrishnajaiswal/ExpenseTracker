9e5ea10b7992ee62a76174db1475fd352b44f4da
manas5996 <manas5996@gmail.com>
2022-11-27 07:44:52 +0530

    After adding dummy expense data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,11 +2,12 @@ import React from "react";
 import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
+import { DUMMY_EXPENSES } from "../../constants/DummyExpenses";
 
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
---
+++ constants/DummyExpenses.js
@@ -0,0 +1,32 @@
+export const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 48.33,
+    date: new Date("2021-12-22"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 48.33,
+    date: new Date("2021-12-22"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 432.33,
+    date: new Date("2022-02-22"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 12.22,
+    date: new Date("2022-04-22"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 22.22,
+    date: new Date("2022-05-22"),
+  },
+];
