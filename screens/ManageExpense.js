461e6d882d239f110e3d8b78af75f9b936bb205f
manas5996 <manas5996@gmail.com>
2022-11-28 13:08:09 +0530

    After deleting and updating expenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,10 +1,12 @@
-import React, { useLayoutEffect } from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
 
 const ManageExpense = ({ route, navigation }) => {
+  const expensesCtx = useContext(ExpensesContext);
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -15,12 +17,26 @@ const ManageExpense = ({ route, navigation }) => {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
   function cancelHandler() {
     navigation.goBack();
   }
   function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: "Test!!!!",
+        amount: 19.99,
+        date: new Date("2022-05-20"),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: "Test",
+        amount: 29.99,
+        date: new Date("2022-05-30"),
+      });
+    }
     navigation.goBack();
   }
   return (
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -9,7 +9,7 @@ const RecentExpenses = () => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
 
-    return expense.date > date7DaysAgo;
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
   return (
     <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
