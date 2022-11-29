ccaad10a43104523dab86ef84e48b5dd7cfaccaf
manas5996 <manas5996@gmail.com>
2022-11-29 14:38:44 +0530

    After working with entered data

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -24,19 +24,11 @@ const ManageExpense = ({ route, navigation }) => {
   function cancelHandler() {
     navigation.goBack();
   }
-  function confirmHandler() {
+  function confirmHandler(expenseData) {
     if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: "Test!!!!",
-        amount: 19.99,
-        date: new Date("2022-05-20"),
-      });
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      expensesCtx.addExpense({
-        description: "Test",
-        amount: 29.99,
-        date: new Date("2022-05-30"),
-      });
+      expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
   }
@@ -45,8 +37,8 @@ const ManageExpense = ({ route, navigation }) => {
       <ExpenseForm
         submitButtonLabel={isEditing ? "Update" : "Add"}
         onCancel={cancelHandler}
+        onSubmit={confirmHandler}
       />
-
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
