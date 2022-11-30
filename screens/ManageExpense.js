63e1faaeb445863445e8f95a3fa9efde125dd63b
manas5996 <manas5996@gmail.com>
2022-11-30 17:27:14 +0530

    After using response data from POST requests

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -28,12 +28,12 @@ const ManageExpense = ({ route, navigation }) => {
   function cancelHandler() {
     navigation.goBack();
   }
-  function confirmHandler(expenseData) {
+  async function confirmHandler(expenseData) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      storeExpense(expenseData);
-      expensesCtx.addExpense(expenseData);
+      const id = await storeExpense(expenseData);
+      expensesCtx.addExpense({ ...expenseData, id: id });
     }
     navigation.goBack();
   }
